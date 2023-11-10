51f10ce56f1b7a8060a48f5fd96ec73d72cea21f
René Eschke <rene@eschke.info>
2023-11-10 14:17:45 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :style="styleObject" class="test">
+    <h1>Why is styleObject [object Object] on initial load during dev?</h1>
+    <h4>although width and height are present?</h4>
+    <h6>But `color: green` is applied immediately?</h6>
+    <pre>width: {{ width }}</pre>
+    <pre>height: {{ height }}</pre>
+    <pre>{{ styleObject }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+
+  data () {
+    return {
+      width: 800,
+      height: 600,
+    }
+  },
+
+  computed: {
+    styleObject () {
+      return {
+        color: 'green',
+        aspectRatio: this.width / this.height,
+      }
+    },
+  },
+}
+</script>
+
+<style>
+.test {
+  width: 100%;
+  background: lightcoral;
+
+  padding: 20px;
+}
+</style>
